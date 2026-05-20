8fb4ddfd8db935c092bdc3360fa2eefd64fa37e7
Elovatorr <Elowynm.wolfaero@gmail.com>
2026-05-19 20:40:37 -0700

    Created main.py

---
+++ Main.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key: str | None = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("failed to load API key")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+result = client.models.generate_content(
+model = "gemini-2.5-flash",
+contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(result.text)
