cbece32327e2d5b47daa444bc1af67ca60e9bf89
Elovatorr <Elowynm.wolfaero@gmail.com>
2026-05-20 22:03:52 -0700

    created run_python_file function

---
+++ functions/run_python_file.py
@@ -0,0 +1,18 @@
+def run_python_file(working_directory: str, file_path: str, args: list[str] | None = None) -> str:
+    try:
+        # directory validation check
+        working_directory_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_directory_abs, file_path))
+        valid_path = os.path.commonpath([working_directory_abs, target_file]) == working_directory_abs
+        if not valid_path:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_file):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+        command = ["python", target_file]
+        if args:
+            command.extend(args)
+
+    except Exception as e:
+        return f"Error: {e}"
