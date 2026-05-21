cbe94cd403fc783586e490ece1396f625af27e21
Elovatorr <Elowynm.wolfaero@gmail.com>
2026-05-21 12:44:34 -0700

    added schema to get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,24 @@
 import os.path
 from config import max_characters
+
+from google.genai import types
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Read the first 10000 characters of content from a file in the working directory as a string. truncates output if contents are longer that 10000 characters",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="File path to the file to be read, relative to the working directory",
+                                    ),
+                                },
+        required=["file_path"]
+    ),
+)
+
+
 def get_file_content(working_directory: str, file_path: str) -> str:
     try:
         working_directory_abs = os.path.abspath(working_directory)
