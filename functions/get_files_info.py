982ce8394b6d07e2cbd662e18d8de8a217accf41
Elovatorr <Elowynm.wolfaero@gmail.com>
2026-05-20 18:11:26 -0700

    added directory parsing in get_files_fino

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -12,6 +12,17 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         if not os.path.isdir(target_directory):
             return f'Error: "{directory}" is not a directory'
 
-        return f'Success: "{directory}" is within the working directory'
+        directory_item_names = os.listdir(target_directory)
+        directory_list = []
+
+        for item in directory_item_names:
+                path_to_item = os.path.join(target_directory,item)
+                item_size = os.path.getsize(path_to_item)
+                #print(path_to_item, item_size)
+                directory_list.append(f"- {item}: file_size={item_size} bytes, is_dir={os.path.isdir(path_to_item)}")
+
+        complete_string = "\n".join(directory_list)
+
+        return complete_string
     except Exception as e:
         return f"Error: {e}"
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,6 +1,9 @@
 from functions.get_files_info import get_files_info
-
+print("result for current directory")
 print(get_files_info("calculator", "."))
+print("result for pkg directory")
+print(get_files_info("calculator", "pkg"))
+print("result for /bin directory")
 print(get_files_info("calculator", "/bin"))
+print("result for ../ directory")
 print(get_files_info("calculator", "../"))
-print(get_files_info("calculator", "main.py"))
