2d7d1d62eb0b877caa940abe83eac7836b652684
Elovatorr <Elowynm.wolfaero@gmail.com>
2026-05-21 17:06:26 -0700

    added a loop for iteration and the bot functions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google.genai import types
 import argparse
 from prompts import *
 from call_function import available_functions, call_function
+import sys
 
 def main():
     load_dotenv()
@@ -17,38 +18,45 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
+
     client = genai.Client(api_key=api_key)
-    user_messages: list[types.Content] = [
+    messages: list[types.Content] = [
         types.Content(role="user",parts=[types.Part(text=args.user_prompt)])
                                      ]
+    for i in range(20):
+        result = client.models.generate_content(
+            model = "gemini-2.5-flash",
+            contents = messages,
+            config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt))
+        if result.candidates:
+            for candidate in result.candidates:
+                messages.append(candidate.content)
 
-    result = client.models.generate_content(
-        model = "gemini-2.5-flash",
-        contents = user_messages,
-        config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt))
+        if result.usage_metadata is None:
+            raise RuntimeError("Usage metadata doesn't exist.")
 
-    if result.usage_metadata is None:
-        raise RuntimeError("Usage metadata doesn't exist idiot.")
+        function_results = []
 
-    function_results = []
+        if result.function_calls:
+            for function in result.function_calls:
+                function_call_result = call_function(function,args.verbose)
+                if not function_call_result.parts:
+                    raise Exception("function call result parts empty.")
+                if not function_call_result.parts[0].function_response:
+                    raise Exception("response is not a FunctionResponse instance.")
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception("no response from function.")
+                if args.verbose is True:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
 
-    if result.function_calls != None:
-        for function in result.function_calls:
-            function_call_result = call_function(function,args.verbose)
-            if not function_call_result.parts:
-                raise Exception("function call result parts empty.")
-            if not function_call_result.parts[0].function_response:
-                raise Exception("response is not a FunctionResponse instance.")
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception("no response from function.")
-            if args.verbose is True:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
+                function_results.append(function_call_result.parts[0])
+            messages.append(types.Content(role="user",parts=function_results))
 
-            function_results.append(function_call_result.parts[0])
-
-    else:
-        print(f"{result.text}")
-
+        else:
+            print(f"{result.text}")
+            return
+    print("maximum iterations reached without a result")
+    sys.exit(1)
 main()
 
 
